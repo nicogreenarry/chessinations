cd4d8067e2e245bdb5b16bae44629017057e7d48
nicogreenarry <@1Y0#ZephFd0>
2020-08-11 22:47:50 -0700

    feat: Standard chess rules

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -1,12 +1,45 @@
+import Chess from 'chess.js';
 import Chessboard from 'chessboardjsx';
-import React, { useReducer } from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { gameStateReducer, initialGameState } from './gameStateReducer';
+export default function Game2() {
+  const [fen, setFen] = useState('start');
+  // square with the currently clicked piece
+  const [pieceSquare, setPieceSquare] = useState('');
+  // array of past game moves
+  const [game, setGame] = useState();
 
-function Game(props) {
-  const [gameState, gameDispatch] = useReducer(gameStateReducer, initialGameState);
+  useEffect(() => setGame(new Chess()), []);
 
-  return <Chessboard position={gameState.position} />;
+  function onDrop({ sourceSquare, targetSquare }) {
+    // see if the move is legal
+    const move = game.move({
+      from: sourceSquare,
+      to: targetSquare,
+      promotion: 'q', // always promote to a queen for example simplicity
+    });
+
+    // illegal move
+    if (move === null) return;
+
+    setFen(game.fen());
+  }
+
+  function onSquareClick(square) {
+    setPieceSquare(square);
+
+    const move = game.move({
+      from: pieceSquare,
+      to: square,
+      promotion: 'q', // always promote to a queen for example simplicity
+    });
+
+    // illegal move
+    if (move === null) return;
+
+    setFen(game.fen());
+    setPieceSquare('');
+  }
+
+  return <Chessboard position={fen} onDrop={onDrop} onSquareClick={onSquareClick} />;
 }
-
-export default Game;
